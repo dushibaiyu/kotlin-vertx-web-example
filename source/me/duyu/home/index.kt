91037c62cfd48a3ce97f4d1cf08f1e6afb66430d
渡世白玉 <dushibaiyu@dushibaiyu.site>
2017-09-23 19:03:51 +0800

    输出线程ID 和开启多线程处理eventloop

--- source/me/duyu/home/index.kt
+++ source/me/duyu/home/index.kt
@@ -1,9 +1,13 @@
 package me.duyu.home
 
 import io.vertx.ext.web.RoutingContext
+import java.lang.Thread.*
+
 
 fun showIndex(context : RoutingContext)
 {
+    val a = currentThread()
+    println(a.id)
     val rep = context.response();
     rep.statusCode = 200;
     rep.end("hello world!")
--- source/me/duyu/home/main.kt
+++ source/me/duyu/home/main.kt
@@ -8,14 +8,31 @@ import io.vertx.core.http.HttpServerOptions
 fun main(args : Array<String>)
 {
     val options = VertxOptions();
+    options.setEventLoopPoolSize(4);
     //options.setWorkerPoolSize(16);
     val vertx = Vertx.vertx(options);
     val httpOptions = HttpServerOptions();
     httpOptions.maxHeaderSize = 16 * 1024;
-    val server = vertx.createHttpServer(httpOptions);
-
     val router = buildRouter(vertx);
-    println("eun the server in 8082");
-    server.requestHandler(router::accept).listen(8082);
 
+    println("start Server Main listen the server in 8082");
+    val mainServer = vertx.createHttpServer(httpOptions);
+    mainServer.requestHandler(router::accept).listen(8082);
+
+    // 开启多个线程处理eventloop
+    for(i in 0 until  options.eventLoopPoolSize) {
+        println("start Server $i listen the server in 8082");
+        val server = vertx.createHttpServer(httpOptions);
+        server.requestHandler(router::accept).listen(8082);
+    }
+    println("main run end!!!");
+
+    /*下面while(true)加上不影响执行，说明main函数执行完上面操作之后一直都是空闲的了，所有的操作都在vertx的线程里处理的。
+    */
+//    while(true)
+//        Thread.sleep(500 * 1000);
+    /*
+    经过调试看出，此线程在执行完毕之后就自动退出了。
+    vertx 默认只启动一个apecct 和 一个eventloop的处理线程，其他几个是请求过来了动态创建的。
+    * */
 }
