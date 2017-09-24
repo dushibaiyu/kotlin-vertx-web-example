df843db13605f045d57b97020ca1e7305a70405f
dushibaiyu <dushibaiyu@yahoo.com>
2017-09-24 13:19:20 +0800

    work 线程

--- source/me/duyu/home/index.kt
+++ source/me/duyu/home/index.kt
@@ -14,3 +14,16 @@ fun showIndex(context : RoutingContext)
     rep.statusCode = 200;
     rep.end("hello world!")
 }
+
+
+fun showIndexSync(context : RoutingContext)
+{
+    val a = currentThread()
+    println("sync thread ID : ${a.id}")
+    Thread.sleep(1000);
+    val req = context.request();
+    println(req.path());
+    val rep = context.response();
+    rep.statusCode = 200;
+    rep.end("hello world!")
+}
--- source/me/duyu/home/router.kt
+++ source/me/duyu/home/router.kt
@@ -6,6 +6,10 @@ import io.vertx.ext.web.Router
 fun buildRouter(vertx : Vertx) : Router
 {
     val router = Router.router(vertx);
-    router.route().handler(::showIndex)
+    // 在eventloop线程中执行
+    router.route("/").handler(::showIndex)
+    // 放到线程池中去执行
+    router.route("/block").blockingHandler(::showIndexSync)
+
     return router;
 }
